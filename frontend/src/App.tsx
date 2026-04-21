3f5d46ab6ab0ad86806cdfec96df400cff8b6e50
hankyul <developer.kyul@gmail.com>
2026-04-21 11:39:33 +0900

    feat : css 확인 링크 라우터 임시 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import AuthPage from './features/auth/components/AuthPage'
 import AttendancePage from './features/attendance/components/AttendancePage'
@@ -8,12 +8,29 @@ import UserPage from './features/user/components/UserPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
-      <Route path="/attendance" element={<AttendancePage />} />
-      <Route path="/leave" element={<LeavePage />} />
-      <Route path="/notice" element={<NoticePage />} />
-      <Route path="/user" element={<UserPage />} />
-    </Routes>
+    <>
+      {/* 메뉴 영역 */}
+      {/* 단순히 페이지 확인용입니다 이용에 참고해주세요 */}
+      <nav>
+        <Link to="/">로그인</Link>
+        <br></br>
+        <Link to="/attendance">출결</Link>
+        <br></br>
+        <Link to="/leave">휴가</Link>
+        <br></br>
+        <Link to="/notice">공지</Link>
+        <br></br>
+        <Link to="/user">사용자</Link>
+      </nav>
+
+      {/* 페이지 연결 영역 */}
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/attendance" element={<AttendancePage />} />
+        <Route path="/leave" element={<LeavePage />} />
+        <Route path="/notice" element={<NoticePage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </>
   )
 }
