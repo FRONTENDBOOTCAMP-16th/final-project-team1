6a8e506c99e94257ee51ae2f2c80489d3c4b5a2e
heeyeon <asd95121@gmail.com>
2026-04-23 10:42:44 +0900

    chore: 대시보드 헤더 레이아웃 구조, css 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
+// import { RouterProvider } from 'react-router-dom'
+// import { router } from './routes'
+
+import { Header } from "./components"
 
 function App() {
-  return <RouterProvider router={router} />
+  // return <RouterProvider router={router} />
+
+  // Header 컴포넌트 작업 중
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App
