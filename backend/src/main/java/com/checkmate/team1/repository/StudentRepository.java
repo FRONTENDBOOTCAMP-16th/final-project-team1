b6af099678b9c36841b011c26e63a2522608bf73
hankyul <developer.kyul@gmail.com>
2026-04-25 15:31:20 +0900

    Reapply "feat. auth 로그인 api 생성"
    This reverts commit 63567e01c34a475d04679f789ecc91918eb6ac52.

--- backend/src/main/java/com/checkmate/team1/repository/StudentRepository.java
+++ backend/src/main/java/com/checkmate/team1/repository/StudentRepository.java
@@ -1,4 +1,11 @@
 package com.checkmate.team1.repository;
 
-public class StudentRepository {
+import com.checkmate.team1.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+    Optional<Student> findByStudentId(String studentId);
 }
