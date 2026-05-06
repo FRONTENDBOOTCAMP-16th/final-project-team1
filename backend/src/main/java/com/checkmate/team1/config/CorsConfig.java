f7848f2a6db89accc87ada2d610a9fd13ebcb8ff
hankyul <developer.kyul@gmail.com>
2026-05-06 13:42:24 +0900

    fix. 백엔드 배포

--- backend/src/main/java/com/checkmate/team1/config/CorsConfig.java
+++ backend/src/main/java/com/checkmate/team1/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://final-project-team1.vercel.app"
+                                "https://final-project-team1.vercel.app",
                                 "https://final-checkmate.netlify.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
