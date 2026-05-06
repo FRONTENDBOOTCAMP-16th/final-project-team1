1148f2c3a8aa77d836675bc7d9debbb305edd49e
hankyul <developer.kyul@gmail.com>
2026-05-06 13:45:19 +0900

    fix: resolve compile error

--- backend/src/main/java/com/checkmate/team1/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/checkmate/team1/exception/GlobalExceptionHandler.java
@@ -1,22 +1,20 @@
 package com.checkmate.team1.exception;
 
-<<<<<<< HEAD
-public class GlobalExceptionHandler {
-}
-=======
 import com.checkmate.team1.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResponse<Void> handleIllegalArgumentException(IllegalArgumentException e) {
+    public ApiResponse<Void> handleIllegalArgumentException(
+            IllegalArgumentException e
+    ) {
 
         return ApiResponse.fail(e.getMessage());
     }
-
 }
->>>>>>> develop
