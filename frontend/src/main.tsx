9fa8939318521c3f34bb1da51cbfe2a10d1b1343
heeyeon <asd95121@gmail.com>
2026-05-15 13:41:18 +0900

    fix: index.css 제거(vite 기본 css코드가 방해요소 역할을 함)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
-import './index.css'
 import './styles/theme/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
